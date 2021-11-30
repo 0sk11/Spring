3ecb5f4e6210eddacceb4c77b86236cc3cf2efd9
0sk11 <itsmeshubhamkumar@gmail.com>
2021-11-30 09:44:45 +0530

    final

--- Spring-Core/src/main/java/spring/core/quest3/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package spring.core.quest3;
-
-public interface BankAccountRepository {
-	public double getBalance(long accountId);
-	public double updateBalance(long accountId, double newBalance);
-}
